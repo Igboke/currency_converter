bea3c8ff8cce7d13568f7c4e86bb4c2e3f0698f3
Igboke Daniel <danieligboke669@gmail.com>
2025-04-29 17:37:54 +0100

    Update ALLOWED_HOSTS to include production and local environments

--- currency_converter/currency_converter/settings.py
+++ currency_converter/currency_converter/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["currency-converter-i7w7.onrender.com","localhost"]
 
 
 # Application definition
