0fe24a3387dbaa23713b2f7e064971dfe5b001f2
Igboke Daniel <danieligboke669@gmail.com>
2025-04-29 12:46:51 +0100

    Add convert currency endpoint to urls.py

--- currency_converter/Apis/urls.py
+++ currency_converter/Apis/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-
+from .views import ConvertCurrencyView
 #Defining endpoints
 urlpatterns =[
+    path("v1/convert/", ConvertCurrencyView.as_view(), name='convert_currency'),
 
 ]
