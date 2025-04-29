2769b5e86a585983d30a4d5f8b1a31661f18912f
Igboke Daniel <danieligboke669@gmail.com>
2025-04-29 14:52:59 +0100

    Add unit tests for CurrencyConversion API with valid and invalid scenarios

--- currency_converter/Apis/tests.py
+++ currency_converter/Apis/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+from rest_framework import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from rest_framework.response import Response
+import datetime
 
-# Create your tests here.
+class CurrencyConversionTestCase(APITestCase):
+    def setUp(self):
+        self.url = reverse('convert_currency')  # Adjust the URL name as needed
+        self.valid_data = {
+            "base_currency": "usd",
+            "converted_currency": "eur",
+            "amount": 100,
+            "date": datetime.datetime.now().strftime("%Y-%m-%d")
+        }
+        self.invalid_data = {
+            "base_currency": "invalid_currency",
+            "converted_currency": "eur",
+            "amount": 100,
+            "date": datetime.datetime.now().strftime("%Y-%m-%d")
+        }
+        self.empty_data = {
+            "base_currency": "",
+            "converted_currency": "",
+            "amount": 0,
+            "date": ""
+        }
+        self.missing_date = {
+            "base_currency": "usd",
+            "converted_currency": "eur",
+            "amount": 100,
+        }
+        self.missing_base_currency = {
+            "converted_currency": "eur",
+            "amount": 100,
+            "date": datetime.datetime.now().strftime("%Y-%m-%d")
+        }
+    
