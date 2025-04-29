ca22e4ab29664f27eadb4930155432c21550dcd3
Igboke Daniel <danieligboke669@gmail.com>
2025-04-29 16:57:00 +0100

    Add endpoint for currency list retrieval

--- currency_converter/Apis/urls.py
+++ currency_converter/Apis/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ConvertCurrencyView
+from .views import ConvertCurrencyView, CurrencyListView
+
 #Defining endpoints
 urlpatterns =[
     path("v1/convert/", ConvertCurrencyView.as_view(), name='convert_currency'),
+    path("v1/currencies/", CurrencyListView.as_view(), name='currency_list'),
 
 ]
