7f9a6cb0dae23428941018776902c6e7d18d607e
Igboke Daniel <danieligboke669@gmail.com>
2025-05-01 18:11:24 +0100

    Remove CurrencyExchangeRateView implementation to streamline API views

--- currency_converter/Apis/views.py
+++ currency_converter/Apis/views.py
@@ -1,79 +1,12 @@
 from rest_framework.views import APIView 
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import CurrencyConversionSerializer, CurrencyConversionOutputSerializer, CurrencyItemSerializer, CurrencyExchangeRateInputSerializer, CurrencyExchangeRateOutputSerializer
+from .serializers import CurrencyConversionSerializer, CurrencyConversionOutputSerializer, CurrencyItemSerializer
 from drf_spectacular.utils import extend_schema
 from .utils import fetch_exchange_rate, FetchRateError, RateNotFoundError, RateCalculationError
 import datetime
 import requests
 import json
-
-# will have to refactor since the code is kinda repeated
-# or make amount not important, if amount isnt added in the currency conversion serializer request it should default to one and just give the plain exchange rate
-class CurrencyExchangeRateView(APIView):
-    """
-    API View to fetch the exchange rate between two currencies (crypto or fiat).
-    """
-    @extend_schema(
-        request=CurrencyExchangeRateInputSerializer,
-        responses={200: CurrencyExchangeRateOutputSerializer},
-        summary="Fetch the exchange rate between two currencies (crypto or fiat).",
-        description="Fetches the latest exchange rate or historical exchange rate based on the provided date."
-    )
-    def post(self, request, *args, **kwargs):
-        input_serializer = CurrencyExchangeRateInputSerializer(data=request.data)
-
-        # Validation
-        if not input_serializer.is_valid():
-            return Response(input_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        # Data is clean now
-        validated_data = input_serializer.validated_data
-        base_currency = validated_data["base_currency"]
-        converted_currency = validated_data["converted_currency"]
-        date = validated_data["date"]
-
-        #utilizing endpoint
-        apiVersion = "v1"
-        # date="latest"
-        endpoint_url = f"https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@{date}/{apiVersion}/currencies/{base_currency}.json"
-        endpoint_url_two = f"https://{date}.currency-api.pages.dev/{apiVersion}/currencies/{base_currency}.json"
-
-
-        try:
-            rate = fetch_exchange_rate(base_currency, converted_currency, date, endpoint_url)
-            
-        except (FetchRateError, RateNotFoundError, RateCalculationError) as e:
-            # If endpoint one failed, store the error and try endpoint two as a fallback
-            
-            last_error = e
-
-            try:
-                # second Attempt
-                rate = fetch_exchange_rate(base_currency, converted_currency, date, endpoint_url_two)
-                
-
-
-            except (FetchRateError, RateNotFoundError, RateCalculationError) as e_two:
-                # If endpoint two also failed, store this error
-                last_error = e_two 
-
-                return Response(
-                    {"error": f"Failed to fetch exchange rate after multiple attempts. Last error: {last_error}"},
-                    status=status.HTTP_500_INTERNAL_SERVER_ERROR
-                )
-        
-        output_data_dict = {
-            "base_currency": base_currency,
-            "converted_currency": converted_currency,
-            "exchange_rate": rate,
-            "date": date,
-        }
-
-        output_serializer = CurrencyExchangeRateOutputSerializer(output_data_dict)
-
-        return Response(output_serializer.data, status=status.HTTP_200_OK)
-
 
 class CurrencyListView(APIView):
     """
